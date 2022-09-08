b0568d0624fad61e71cb0886ad039ae64fb0dfa8
DETA SARI ASLINA <112565733+Detasariaslina09@users.noreply.github.com>
2022-09-08 18:48:34 +0700

    Add files via upload

---
+++ DETA SARI ASLINA-2217051117.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int sisi,keliling,luas;
+	
+	cout<<"masukan sisi persegi : ";
+	cin>> sisi;
+	keliling=4*sisi;
+	cout<<"keliling persegi adalah"<<keliling<<endl;
+	
+	luas=sisi*sisi;
+	cout<<"luas persegi adalah"<<luas;
+	return 0;
+
+}
